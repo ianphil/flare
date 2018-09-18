5335fb78395762487789967d7cc3ef554a051783
Ian Philpot <ian.philpot@microsoft.com>
2018-09-18 16:50:12 -0400

    Get Storage Account Key[s]

--- apis/storage/storage.go
+++ apis/storage/storage.go
@@ -3,28 +3,32 @@ package storage
 import (
 	"context"
 	"fmt"
-	"log"
+
+	"github.com/iphilpot/flare/apis/common"
+	"github.com/iphilpot/flare/apis/config"
+	"github.com/iphilpot/flare/apis/iam"
 
 	"github.com/Azure/azure-sdk-for-go/services/preview/storage/mgmt/2018-03-01-preview/storage"
 )
 
 func getStorageAccountClient() storage.AccountsClient {
-	storageAccountClient := storage.NewAccountsClient("")
-
+	configInfo := config.GetConfig()
+	storageAccountClient := storage.NewAccountsClient(configInfo.AzureSubscriptionID)
+	storageAccountClient.Authorizer = iam.GetAuthorizerFromEnvironment()
 	return storageAccountClient
 }
 
+// GetAccountKeys - Returns keys for specified account
+func GetAccountKeys(ctx context.Context, accountName, accountGroupName string) (storage.AccountListKeysResult, error) {
+	client := getStorageAccountClient()
+	return client.ListKeys(ctx, accountGroupName, accountName)
+}
+
 // GetStorageAccountPrimaryKey - Return primary key of storage account.
-func GetStorageAccountPrimaryKey(ctx context.Context, storageAccountClient *storage.AccountsClient, accountName, accountGroupName string) (string, error) {
-	// Get Primary storage account key
-	keyResult, err := storageAccountClient.ListKeys(ctx, accountGroupName, accountName)
-	if err != nil {
-		log.Println(err)
-	}
-
+func GetStorageAccountPrimaryKey(ctx context.Context, accountName, accountGroupName string) string {
+	keyResult, err := GetAccountKeys(ctx, accountGroupName, accountName)
+	common.HandleError(err)
 	primaryKey := *(((*keyResult.Keys)[0]).Value)
-
-	fmt.Printf("Primary storage account key: %s\n", primaryKey)
-
-	return primaryKey, err
+	common.PrintAndLog(fmt.Sprintf("Primary storage account key: %s\n", primaryKey))
+	return primaryKey
 }
