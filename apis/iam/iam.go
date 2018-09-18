d2534e56a4ce623f3b330550b704e756f09f1e52
Ian Philpot <ian.philpot@microsoft.com>
2018-09-18 16:34:09 -0400

    Switch error handling for package

--- apis/iam/iam.go
+++ apis/iam/iam.go
@@ -1,19 +1,16 @@
 package iam
 
 import (
-	"github.com/iphilpot/flare/apis/common"
-
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/iphilpot/flare/apis/common"
 )
 
 // GetAuthorizerFromEnvironment - returns authorizer from Env Vars
 func GetAuthorizerFromEnvironment() autorest.Authorizer {
 	// create an authorizer from env vars or Azure Managed Service Identity
 	authorizer, err := auth.NewAuthorizerFromEnvironment()
-	if err != nil {
-		common.PrintAndLog("GetAuthorizerFromEnvironment - Not Authorized")
-	}
+	common.HandleError(err)
 
 	return authorizer
 }
